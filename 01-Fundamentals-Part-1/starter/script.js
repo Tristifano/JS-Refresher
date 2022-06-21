5aff3b6d0d35efbeffde8f392fbf093939c9bf11
Tristifano <Tristanbmyers@gmail.com>
2022-06-21 16:44:49 -0400

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -202,21 +202,41 @@ console.log(now - 1991 > now - 2018)
 // }
 
 // type conversion
-const inputYear = '1991'
-console.log(Number(inputYear), inputYear)
-console.log(Number(inputYear) + 18)
+// const inputYear = '1991'
+// console.log(Number(inputYear), inputYear)
+// console.log(Number(inputYear) + 18)
 
-console.log(Number('Jonas'))
-console.log(typeof NaN)
+// console.log(Number('Jonas'))
+// console.log(typeof NaN)
 
-console.log(String(23), 23)
+// console.log(String(23), 23)
 
-//type coersion
-console.log('I am ' + 23 + ' years old')
-console.log('23' - '10' - 3)
-console.log('23' / '2')
+// //type coersion
+// console.log('I am ' + 23 + ' years old')
+// console.log('23' - '10' - 3)
+// console.log('23' / '2')
 
-let n = '1' + 1
-console.log(n)
-n = n - 1
-console.log(n)
+// let n = '1' + 1
+// console.log(n)
+// n = n - 1
+// console.log(n)
+
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean('Jonas'))
+console.log(Boolean({}))
+console.log(Boolean(''))
+
+const money = 100
+if (money) {
+    console.log(`Don't spend it all ;)`)
+} else {
+    console.log('You should get a job!')
+}
+
+let height = 68
+if (height) {
+    console.log('YAY! Height is defined')
+} else {
+    console.log('Height is UNDEFINED')
+}
