a2f8a3d18f05df3d2177c425eda56bf49a09e8b9
Tristifano <Tristanbmyers@gmail.com>
2022-06-21 17:22:49 -0400

    Coding Challenge #3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -262,18 +262,48 @@ console.log(now - 1991 > now - 2018)
 
 // if (favorite !== 23) console.log('Why not 23?')
 
-const hasDriversLicense = true
-const hasGoodVision = true
+// const hasDriversLicense = true
+// const hasGoodVision = true
 
-console.log(hasDriversLicense && hasGoodVision)
-console.log(hasDriversLicense || hasGoodVision)
-console.log(!hasDriversLicense)
+// console.log(hasDriversLicense && hasGoodVision)
+// console.log(hasDriversLicense || hasGoodVision)
+// console.log(!hasDriversLicense)
 
-const isTired = false
-console.log(hasDriversLicense && hasGoodVision && isTired)
+// const isTired = false
+// console.log(hasDriversLicense && hasGoodVision && isTired)
 
-if (hasDriversLicense && hasGoodVision && !isTired) {
-    console.log('Sarah is able to drive!')
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//     console.log('Sarah is able to drive!')
+// } else {
+//     console.log('Someone else should drive')
+// }
+
+// Coding Challenge #3
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+// 1. Calculatetheaveragescoreforeachteam,usingthetestdatabelow
+// 2. Comparetheteam'saveragescorestodeterminethewinnerofthecompetition, and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus1:Includearequirementforaminimumscoreof100.Withthisrule,a
+// team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. Hint: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+// 4. Bonus2:Minimumscorealsoappliestoadraw!Soadrawonlyhappenswhen both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123 § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+// GOOD LUCK 😀
+
+// const averageDolphins = (96 + 108 + 89) / 3
+// const averageKoalas = (88 + 91 + 110) / 3
+// const averageDolphins = (97 + 112 + 101) / 3
+// const averageKoalas = (109 + 95 + 123) / 3
+const averageDolphins = (97 + 112 + 101) / 3
+const averageKoalas = (109 + 95 + 106) / 3
+
+console.log(averageDolphins, averageKoalas)
+if (averageDolphins === averageKoalas) {
+    console.log('They have the same average score')
+} else if (averageDolphins > averageKoalas) {
+    console.log('The Dolphins have a higher average')
 } else {
-    console.log('Someone else should drive')
+    console.log('The Koalas have a higher average')
 }
