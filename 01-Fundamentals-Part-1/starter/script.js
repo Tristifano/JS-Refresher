2f76a89cbe793c08d5cc6024c15c8fb34633a4db
Tristifano <Tristanbmyers@gmail.com>
2022-06-21 17:00:58 -0400

    Equality operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -221,22 +221,43 @@ console.log(now - 1991 > now - 2018)
 // n = n - 1
 // console.log(n)
 
-console.log(Boolean(0))
-console.log(Boolean(undefined))
-console.log(Boolean('Jonas'))
-console.log(Boolean({}))
-console.log(Boolean(''))
+// console.log(Boolean(0))
+// console.log(Boolean(undefined))
+// console.log(Boolean('Jonas'))
+// console.log(Boolean({}))
+// console.log(Boolean(''))
 
-const money = 100
-if (money) {
-    console.log(`Don't spend it all ;)`)
+// const money = 100
+// if (money) {
+//     console.log(`Don't spend it all ;)`)
+// } else {
+//     console.log('You should get a job!')
+// }
+
+// let height = 68
+// if (height) {
+//     console.log('YAY! Height is defined')
+// } else {
+//     console.log('Height is UNDEFINED')
+// }
+
+const age = '18';
+if (age === 18) console.log('You became an adult :D (strict)')
+
+if (age == 18) console.log('You became an adult :D (loose)')
+
+const favorite = Number(prompt(`What's your favorite number?`))
+console.log(favorite)
+console.log(typeof favorite)
+
+if (favorite === 23) {
+    console.log('Cool! 23 is an amazing number!')
+} else if (favorite === 7) {
+    console.log('7 is also a cool number')
+} else if (favorite === 9) {
+    console.log('9 is also a cool number')
 } else {
-    console.log('You should get a job!')
+    console.log('Number is not 23 or 7')
 }
 
-let height = 68
-if (height) {
-    console.log('YAY! Height is defined')
-} else {
-    console.log('Height is UNDEFINED')
-}
+if (favorite !== 23) console.log('Why not 23?')
