ba9c21fb655148ca725a036b93ec10160031864b
Tristifano <Tristanbmyers@gmail.com>
2022-06-22 17:34:53 -0400

    object methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -211,44 +211,73 @@ Apply this to the team's average scores 😉 GOOD LUCK 😀
 // console.log(tips)
 // console.log(totals)
 
-const jonasArray = [
-    'Jonas',
-    'Schmedtmann',
-    2037 - 1991,
-    'teacher',
-    ['Michael', 'Peter', 'Steven']
-]
+// const jonasArray = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven']
+// ]
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// }
+// console.log(jonas)
+
+// console.log(jonas.lastName)
+// console.log(jonas['lastName'])
+// const nameKey = 'Name'
+// console.log(jonas['first' + nameKey])
+// console.log(jonas['last' + nameKey])
+
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, friends')
+// console.log(jonas[interestedIn])
+
+// if (jonas[interestedIn]) {
+//     console.log(jonas[interestedIn])
+// } else {
+//     console.log('Wrong request! Choose between firstName, lastName, age, job, friends')
+// }
+
+// jonas.location = 'Portugal'
+// jonas['twitter'] = '@jonasschemdtmann'
+// console.log(jonas)
+
+// //Challenge
+// // Jonas has 3 friends, and his best friend is Michael
+
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
 
 const jonas = {
     firstName: 'Jonas',
     lastName: 'Schedtmann',
-    age: 2037 - 1991,
+    birthYeah: 1991,
     job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven']
-}
-console.log(jonas)
-
-console.log(jonas.lastName)
-console.log(jonas['lastName'])
-const nameKey = 'Name'
-console.log(jonas['first' + nameKey])
-console.log(jonas['last' + nameKey])
-
-const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, friends')
-console.log(jonas[interestedIn])
-
-if (jonas[interestedIn]) {
-    console.log(jonas[interestedIn])
-} else {
-    console.log('Wrong request! Choose between firstName, lastName, age, job, friends')
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+    // calcAge: function (birthYeah) {
+    //     return 2037 - birthYeah
+    // }
+    calcAge: function () {
+        this.age = 2037 - this.birthYeah
+        return this.age
+    },
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()} year old teacher, and ${jonas.hasDriversLicense ? 'he has a drivers license' : 'he does not have a drivers license'}`
+    }
 }
 
-jonas.location = 'Portugal'
-jonas['twitter'] = '@jonasschemdtmann'
-console.log(jonas)
+console.log(jonas.calcAge())
 
-//Challenge
-// Jonas has 3 friends, and his best friend is Michael
+console.log(jonas.age)
+console.log(jonas.age)
+console.log(jonas.age)
 
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
+// Challenge
+// Jonas is a 46 year old teacher, and he has a driver's license"
 
+console.log(jonas.getSummary())
