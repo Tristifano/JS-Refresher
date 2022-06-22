e0cfa0c4921088b2e3e2b5c283e485bcb4e30877
Tristifano <Tristanbmyers@gmail.com>
2022-06-22 14:06:48 -0400

    added Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,9 +1,29 @@
 'use strict'
 
-let hasDriversLicense = false
-const passTest = true
-if (passTest) hasDriverLicense = true
-if (hasDriversLicense) console.log('I can drive :D')
+// let hasDriversLicense = false
+// const passTest = true
+// if (passTest) hasDriverLicense = true
+// if (hasDriversLicense) console.log('I can drive :D')
 
 // const interface = 'Audio'
 // const private = 534
+function logger() {
+    console.log('My name is Jonas')
+}
+
+logger()
+logger()
+logger()
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice
+}
+
+const appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice)
+
+const appleOrangeJuice = fruitProcessor(2, 4)
+console.log(appleOrangeJuice)
+
+const num = Number('23')
