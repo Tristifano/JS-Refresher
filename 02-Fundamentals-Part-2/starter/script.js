f7f7418c5496091904b41d0f96d95a91196a38c7
Tristifano <Tristanbmyers@gmail.com>
2022-06-22 16:15:45 -0400

    coding challenge #5

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -164,30 +164,49 @@ Apply this to the team's average scores 😉 GOOD LUCK 😀
 // console.log(ages)
 
 // Add Elements
-const friends = ['Michael', 'Steven', 'Peter']
-friends.push('Jay')
-console.log(friends)
+// const friends = ['Michael', 'Steven', 'Peter']
+// friends.push('Jay')
+// console.log(friends)
 
-friends.unshift('John')
-console.log(friends)
+// friends.unshift('John')
+// console.log(friends)
 
-// Remove elements 
-friends.pop() // last
-const popped = friends.pop()
-console.log(popped)
-console.log(friends)
+// // Remove elements 
+// friends.pop() // last
+// const popped = friends.pop()
+// console.log(popped)
+// console.log(friends)
 
-friends.shift() //first
-console.log(friends)
+// friends.shift() //first
+// console.log(friends)
 
-console.log(friends.indexOf('Steven'))
-console.log(friends.indexOf('Bob'))
+// console.log(friends.indexOf('Steven'))
+// console.log(friends.indexOf('Bob'))
 
-friends.push(23)
-console.log(friends.includes('Steven'))
-console.log(friends.includes('Bob'))
-console.log(friends.includes(23))
+// friends.push(23)
+// console.log(friends.includes('Steven'))
+// console.log(friends.includes('Bob'))
+// console.log(friends.includes(23))
 
-if (friends.includes('Steven')) {
-    console.log('You have a friend named Steven')
-}
+// if (friends.includes('Steven')) {
+//     console.log('You have a friend named Steven')
+// }
+
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+// Your tasks:
+// 1. Writeafunction'calcTip'thattakesanybillvalueasaninputandreturns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+// 2. Andnowlet'susearrays!Socreateanarray'bills'containingthetestdata below
+// 3. Createanarray'tips'containingthetipvalueforeachbill,calculatedfrom the function you created before
+// 4. Bonus:Createanarray'total'containingthetotalvalues,sothebill+tip Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+// GOOD LUCK 😀
+
+const calcTip = (bill) => bill >= 50 && bill <= 300 ? bill * .15 : bill * .2
+
+const bills = new Array(125, 555, 44)
+const tips = new Array(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]))
+const totals = new Array(bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2])
+console.log(bills)
+console.log(tips)
+console.log(totals)
