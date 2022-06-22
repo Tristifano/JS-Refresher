a0bfb314deb26dddf84f5b05f97b274f0330cca2
Tristifano <Tristanbmyers@gmail.com>
2022-06-22 14:22:30 -0400

    Arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -7,23 +7,50 @@
 
 // const interface = 'Audio'
 // const private = 534
-function logger() {
-    console.log('My name is Jonas')
+// function logger() {
+//     console.log('My name is Jonas')
+// }
+
+// logger()
+// logger()
+// logger()
+
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+//     return juice
+// }
+
+// const appleJuice = fruitProcessor(5, 0)
+// console.log(appleJuice)
+
+// const appleOrangeJuice = fruitProcessor(2, 4)
+// console.log(appleOrangeJuice)
+
+// const num = Number('23')
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear
 }
 
-logger()
-logger()
-logger()
+const age1 = calcAge1(1991)
+console.log(age1)
 
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
-    return juice
+const calcAge2 = function (birthYeah) {
+    return 2037 - birthYeah
 }
 
-const appleJuice = fruitProcessor(5, 0)
-console.log(appleJuice)
+const age2 = calcAge2(1991)
+console.log(age1, age2)
 
-const appleOrangeJuice = fruitProcessor(2, 4)
-console.log(appleOrangeJuice)
+const calcAge3 = birthYeah => 2037 - birthYeah
+const age3 = calcAge3(1991)
+console.log(age3)
 
-const num = Number('23')
+const yearsUntilRetirement = (birthYeah, firstName) => {
+    const age = 2037 - birthYeah
+    const retirement = 65 - age
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'))
+console.log(yearsUntilRetirement(1980, 'Bob'))
