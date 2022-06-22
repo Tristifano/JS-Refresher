336ba613a21d09cc8a96aa86b04fe372a7997554
Tristifano <Tristanbmyers@gmail.com>
2022-06-22 16:47:03 -0400

    intro to objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -202,11 +202,27 @@ Apply this to the team's average scores 😉 GOOD LUCK 😀
 // Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
 // GOOD LUCK 😀
 
-const calcTip = (bill) => bill >= 50 && bill <= 300 ? bill * .15 : bill * .2
+// const calcTip = (bill) => bill >= 50 && bill <= 300 ? bill * .15 : bill * .2
 
-const bills = new Array(125, 555, 44)
-const tips = new Array(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]))
-const totals = new Array(bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2])
-console.log(bills)
-console.log(tips)
-console.log(totals)
+// const bills = new Array(125, 555, 44)
+// const tips = new Array(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]))
+// const totals = new Array(bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2])
+// console.log(bills)
+// console.log(tips)
+// console.log(totals)
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+]
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+}
