3ce7d5af9a00efe635b606ce1d520d131429571c
Tristifano <Tristanbmyers@gmail.com>
2022-06-23 11:47:36 -0400

    For loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -309,3 +309,18 @@ Apply this to the team's average scores 😉 GOOD LUCK 😀
 // }
 
 // compareBMIs()
+
+// console.log('Lifting weights repetition 1 🏋️‍♀️')
+// console.log('Lifting weights repetition 2 🏋️‍♀️')
+// console.log('Lifting weights repetition 3 🏋️‍♀️')
+// console.log('Lifting weights repetition 4 🏋️‍♀️')
+// console.log('Lifting weights repetition 5 🏋️‍♀️')
+// console.log('Lifting weights repetition 6 🏋️‍♀️')
+// console.log('Lifting weights repetition 7 🏋️‍♀️')
+// console.log('Lifting weights repetition 8 🏋️‍♀️')
+// console.log('Lifting weights repetition 9 🏋️‍♀️')
+// console.log('Lifting weights repetition 10 🏋️‍♀️')
+
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`)
+}
