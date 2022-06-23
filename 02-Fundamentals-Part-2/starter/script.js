f2373dbf8f651423a4f686dadfc4b0a3e712cdea
Tristifano <Tristanbmyers@gmail.com>
2022-06-23 12:17:30 -0400

    Looping arrays breaking and continue

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -321,6 +321,45 @@ Apply this to the team's average scores 😉 GOOD LUCK 😀
 // console.log('Lifting weights repetition 9 🏋️‍♀️')
 // console.log('Lifting weights repetition 10 🏋️‍♀️')
 
-for (let rep = 1; rep <= 10; rep++) {
-    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`)
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`)
+// }
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+]
+
+const types = []
+
+for (let i = 0; i < jonas.length; i++) {
+    console.log(jonas[i], typeof jonas[i])
+    types[i] = typeof jonas[i]
+}
+console.log(types)
+
+const years = [1991, 2007, 1969, 2020]
+const ages = []
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i])
+}
+console.log(ages)
+
+// continue and break
+
+console.log('---Only strings---')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue
+    console.log(jonas[i], typeof jonas[i])
+}
+
+console.log('---Only numbers---')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break
+    console.log(jonas[i], typeof jonas[i])
 }
