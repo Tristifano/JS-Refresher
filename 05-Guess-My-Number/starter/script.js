0baaf71a925167f9c3131497d0dbcf5652099338
Tristifano <Tristanbmyers@gmail.com>
2022-07-07 17:02:37 -0400

    added reset functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,11 +8,23 @@
 // console.log(document.querySelector('.guess').value);
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+const resetGame = () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.highscore').textContent = score;
+  score = 0;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+};
+
+document.querySelector('.again').addEventListener('click', resetGame);
 
 document.querySelector('.check').addEventListener('click', function () {
   console.log(document.querySelector('.guess').value);
@@ -22,6 +34,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔️ No number';
   } else if (guess === secretNumber) {
     // When Player Wins
+    document.querySelector('.number').textContent = secretNumber;
+
     document.querySelector('.message').textContent = '🎉 Correct Number!';
 
     document.querySelector('body').style.backgroundColor = '#60b347';
