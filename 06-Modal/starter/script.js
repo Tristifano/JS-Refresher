30c845bad192a7d10cdf572ac39ebe3dd1b30e6d
Tristifano <Tristanbmyers@gmail.com>
2022-07-12 17:43:56 -0400

    working with classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -7,5 +7,18 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
